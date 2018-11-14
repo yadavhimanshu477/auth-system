e299d66bcf36a195610404a4c85f81474f4fa210
himanshu <himanshu.yadav@sendinblue.com>
2018-11-14 20:44:01 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var fs = require('fs');
+
+// invoke an instance of express application.
+var app = express();
+
+// set our application port
+app.set('port', 9000);
+
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 6000
+    }
+}));
+
+var rawdata = fs.readFileSync('userData.json');  
+var userData = JSON.parse(rawdata);
+
+console.log(userData)
+
+function insertDataJson(data) {
+	var userData = JSON.stringify(data);
+	fs.writeFileSync('userData.json', userData); 
+}
+
+
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
+
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }    
+};
+
+
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
+    })
+    .post((req, res) => {
+        insertDataJson({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.redirect('/login')
+    });
+
+app.route('/forgetPassword')
+	.get(sessionChecker, (req, res) => {
+	    res.sendFile(__dirname + '/public/forget.html');
+	})
+	.post((req, res) => {
+		username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        confirm_password : req.body.confirm_password
+
+		if (username == userData.username && email == userData.email) {
+		    insertDataJson({
+		        username: req.body.username,
+		        email: req.body.email,
+		        password: req.body.password,
+		        confirm_password : req.body.confirm_password
+		    })
+		    res.redirect('/login')
+		} else {
+	    	res.redirect('/forgetPassword')
+	    }
+	});
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        if (!((userData.username == username) && (userData.password == password))) {
+        	console.log("username or password not matched.")
+            res.redirect('/login');
+        } else {
+            req.session.user = userData.username;
+            res.redirect('/dashboard');
+        }
+    });
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/dashboard.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for handling 404 requests(unavailable routes)
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+// start the express server
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
